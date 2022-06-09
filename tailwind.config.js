0e3b3b45e80f69b09d5bcb8361663064db916942
Felix Pacheco <felix.pacheco@zeppelinlabs.io>
2022-06-09 13:08:11 -0400

    Actualiza tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,22 @@ module.exports = {
         './components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
+        textColor: (theme) => ({
+            ...theme('colors'),
+            react: '#54def2',
+        }),
+        borderColor: (theme) => ({
+            ...theme('colors'),
+            react: '#54def2',
+        }),
+        backgroundColor: (theme) => ({
+            ...theme('colors'),
+            react: '#54def2',
+        }),
         extend: {
             minHeight: {
                 '90-screen': '90vh',
+                'screen-3/4': '75vh',
             },
         },
     },
