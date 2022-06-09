daba36ab316c60ce14d4695f33f6bb4d1af2307c
Felix Pacheco <felix.pacheco@zeppelinlabs.io>
2022-06-09 10:57:33 -0400

    Actualiza config de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 module.exports = {
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+    },
     content: [
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            minHeight: {
+                '90-screen': '90vh',
+            },
+        },
     },
     plugins: [],
 };
