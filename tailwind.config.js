d6e624b41a28ea0ad99a9e7eac8a3e3b8acbdead
Felix Pacheco <felix.pacheco@zeppelinlabs.io>
2022-06-12 13:50:43 -0400

    Configura breakpoints y colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
     theme: {
         textColor: (theme) => ({
             ...theme('colors'),
+            'custom-purple': '#582DE8',
             react: '#54def2',
         }),
         borderColor: (theme) => ({
@@ -18,8 +19,25 @@ module.exports = {
         }),
         backgroundColor: (theme) => ({
             ...theme('colors'),
+            'custom-black': '#080a10',
+            'custom-purple': '#582DE8',
             react: '#54def2',
         }),
+        screens: {
+            mobileXs: '350px',
+            mobileSm: '420px',
+            mobileMd: '480px',
+            mobile: '520px',
+            mobileLg: '600px',
+            mobileXl: '680px',
+            tablet: '762px',
+            tabletLg: '880px',
+            tabletXl: '952px',
+            laptop: '1024px',
+            laptopLg: '1180px',
+            laptopXl: '1260px',
+            desktop: '1344px',
+        },
         extend: {
             minHeight: {
                 '90-screen': '90vh',
