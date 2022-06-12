35174b04c86f21c7bbe0b32e408dae2e9cd27364
Felix Pacheco <felix.pacheco@zeppelinlabs.io>
2022-06-12 13:54:15 -0400

    Implementa experience

---
+++ components/experience/Experience.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { TExperience, getTextColor } from './helpers';
+
+type Props = {
+    currentExperience: TExperience,
+}
+
+const getDefaultStyles = (isActual?: boolean) => {
+    if (isActual) {
+        return 'p-8 transition-all duration-500 ease bg-slate-100 text-slate-700';
+    }
+    return 'p-8 transition-all duration-500 ease hover:bg-slate-100 hover:text-slate-700';
+};
+
+const Experience = ({ currentExperience }: Props) => (
+    <div className={getDefaultStyles(currentExperience.isCurrent)}>
+        <span className="block text-sm">{currentExperience.period}</span>
+        <span className="block text-xl font-semibold">{currentExperience.charge}</span>
+        <span className={`text-sm ${getTextColor(currentExperience.enterprise.name)}`}>
+            {currentExperience.enterprise.name}
+        </span>
+        <p className="mt-6 text-sm">{currentExperience.resume}</p>
+    </div>
+);
+
+export default Experience;
