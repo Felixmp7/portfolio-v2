b1a7180253f214ce188d0b88af80f7648d1227d0
Felix Pacheco <felix.pacheco@zeppelinlabs.io>
2022-06-12 13:55:19 -0400

    Importa experiencias y call to action

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,17 @@ import type { NextPage } from 'next';
 import PageHead from '@components/PageHead';
 import Welcome from '@components/Welcome';
 import PersonalInfo from '@components/PersonalInfo';
+import Experiences from '@components/experience/Experiences';
+import CallToAction from '@components/CallToAction';
 
 const Home: NextPage = () => (
-    <div className="h-screen bg-black">
+    <>
         <PageHead />
         <Welcome />
         <PersonalInfo />
-        {/* <footer className="">
-            <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Powered by
-                {' '}
-                <span className="">
-                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                </span>
-            </a>
-        </footer> */}
-    </div>
+        <Experiences />
+        <CallToAction />
+    </>
 );
 
 export default Home;
