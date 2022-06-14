4727d40ea821d2d4ab17cd1b3afb2ebb45578e15
Felix Pacheco <felix.pacheco@zeppelinlabs.io>
2022-06-14 18:09:11 -0400

    Configura traducciones

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { i18n } = require('./next-i18next.config');
+
 const nextConfig = {
+    i18n,
     reactStrictMode: true,
 };
 
