15ac95538d667fa840e8d29b82e55110af1b600e
Felix Pacheco <felix.pacheco@zeppelinlabs.io>
2022-06-14 18:10:07 -0400

    Exporta getStaticProps para traducciones

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import PageHead from '@components/PageHead';
 import Welcome from '@components/Welcome';
 import PersonalInfo from '@components/PersonalInfo';
 import Experiences from '@components/experience/Experiences';
 import CallToAction from '@components/CallToAction';
 import LatestWork from '@components/LatestWork';
+
+export const getStaticProps = async ({ locale }: { locale: string }) => ({
+    props: {
+        ...await serverSideTranslations(locale, ['common']),
+    },
+});
 
 const Home: NextPage = () => (
     <>
