e6b80f4553553557872f82340bfe217a053678d2
Felix Pacheco <devfex.19@gmail.com>
2022-09-21 10:35:59 -0400

    Elimina comentarios

--- types.ts
+++ types.ts
@@ -9,62 +9,6 @@ export enum eEntrerprises {
 
 }
 
-// type TImageFormat = {
-//     name: string,
-//     hash: string,
-//     ext: string,
-//     mime: string,
-//     path: null,
-//     width: number,
-//     height: number,
-//     size: number,
-//     url: string
-// }
-
-// export type TImage = {
-//     id: number,
-//     attributes: {
-//         name: string,
-//         alternativeText: string,
-//         caption: string,
-//         width: number,
-//         height: number,
-//         formats: {
-//             thumbnail: TImageFormat,
-//             large: TImageFormat,
-//             medium: TImageFormat,
-//             small: TImageFormat
-//         },
-//         hash: string,
-//         ext: string,
-//         mime: string,
-//         size: number,
-//         url: string,
-//         previewUrl: null,
-//         provider: string,
-//         provider_metadata: null,
-//         createdAt: string,
-//         updatedAt: string
-//     },
-// }
-
-// export type TLatestWork = {
-//     id: number,
-//     attributes: {
-//         images: {
-//             data: TImage[]
-//         }
-//     }
-// }
-
-// export type TStrapiResponse = {
-//     data: TLatestWork[],
-//     meta: never,
-//     error?: {
-//         status: number
-//     }
-// }
-
 export type TEnterprise = {
     slug: eEntrerprises,
     logo: StaticImageData,
